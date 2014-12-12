90bde90041f49b3f5d08377c50b355d34393c71a
camierjs <nabla@x-quad>
2014-12-12 09:53:39 +0100

    [141212] CUDA extra cleanup

--- backends/cuda/outputs/extra.h
+++ backends/cuda/outputs/extra.h
@@ -17,94 +17,10 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // See the LICENSE file for details.
 
-#define cudaCalloc(devPtr,size)                                         \
-  do{                                                                   \
-    if (cudaSuccess==cudaMalloc(devPtr,size))                           \
-      cudaMemset(*devPtr,0,size);                                       \
-  }while(0)
-
-
 // *****************************************************************************
 // * EXTRA
 // *****************************************************************************
 #define MINEQ(a,b) (a)=(((a)<(b))?(a):(b))
-__device__ void reduceMin(Real *sresult, const int threadID){
-    /* If number of threads is not a power of two, first add the ones
-       after the last power of two into the beginning. At most one of
-       these conditionals will be true for a given NPOT block size. */
-  if (CUDA_NB_THREADS_PER_BLOCK > 512 && CUDA_NB_THREADS_PER_BLOCK <= 1024){
-    __syncthreads();
-    if (threadID < CUDA_NB_THREADS_PER_BLOCK-512)
-      MINEQ(sresult[threadID],sresult[threadID + 512]);
-  }
-  if (CUDA_NB_THREADS_PER_BLOCK > 256 && CUDA_NB_THREADS_PER_BLOCK < 512){
-    __syncthreads();
-    if (threadID < CUDA_NB_THREADS_PER_BLOCK-256)
-      MINEQ(sresult[threadID],sresult[threadID + 256]);
-  }
-  if (CUDA_NB_THREADS_PER_BLOCK > 128 && CUDA_NB_THREADS_PER_BLOCK < 256){
-    __syncthreads();
-    if (threadID < CUDA_NB_THREADS_PER_BLOCK-128)
-      MINEQ(sresult[threadID],sresult[threadID + 128]);
-  }
-  if (CUDA_NB_THREADS_PER_BLOCK > 64 && CUDA_NB_THREADS_PER_BLOCK < 128){
-    __syncthreads();
-    if (threadID < CUDA_NB_THREADS_PER_BLOCK-64)
-      MINEQ(sresult[threadID],sresult[threadID + 64]);
-  }
-  if (CUDA_NB_THREADS_PER_BLOCK > 32 && CUDA_NB_THREADS_PER_BLOCK < 64){
-    __syncthreads();
-    if (threadID < CUDA_NB_THREADS_PER_BLOCK-32)
-      MINEQ(sresult[threadID],sresult[threadID + 32]);
-  }
-  if (CUDA_NB_THREADS_PER_BLOCK > 16 && CUDA_NB_THREADS_PER_BLOCK < 32){
-    __syncthreads();
-    if (threadID < CUDA_NB_THREADS_PER_BLOCK-16)
-      MINEQ(sresult[threadID],sresult[threadID + 16]);
-  }
-  if (CUDA_NB_THREADS_PER_BLOCK > 8 && CUDA_NB_THREADS_PER_BLOCK < 16){
-    __syncthreads();
-    if (threadID < CUDA_NB_THREADS_PER_BLOCK-8)
-      MINEQ(sresult[threadID],sresult[threadID + 8]);
-  }
-  if (CUDA_NB_THREADS_PER_BLOCK > 4 && CUDA_NB_THREADS_PER_BLOCK < 8){
-    __syncthreads();
-    if (threadID < CUDA_NB_THREADS_PER_BLOCK-4)
-      MINEQ(sresult[threadID],sresult[threadID + 4]);
-  }
-  if (CUDA_NB_THREADS_PER_BLOCK > 2 && CUDA_NB_THREADS_PER_BLOCK < 4){
-    __syncthreads();
-    if (threadID < CUDA_NB_THREADS_PER_BLOCK-2)
-      MINEQ(sresult[threadID],sresult[threadID + 2]);
-  }
-  if (CUDA_NB_THREADS_PER_BLOCK >= 512){
-    __syncthreads();
-    if (threadID < 256)
-      MINEQ(sresult[threadID],sresult[threadID + 256]);
-  }
-    if (CUDA_NB_THREADS_PER_BLOCK >= 256){
-        __syncthreads();
-        if (threadID < 128)
-          MINEQ(sresult[threadID],sresult[threadID + 128]);
-    }
-    if (CUDA_NB_THREADS_PER_BLOCK >= 128){
-      __syncthreads();
-      if (threadID < 64)
-        MINEQ(sresult[threadID],sresult[threadID + 64]);
-    }
-    __syncthreads();
-    if (threadID < 32) {
-      volatile Real *vol = sresult;
-      if (CUDA_NB_THREADS_PER_BLOCK >= 64) MINEQ(vol[threadID],vol[threadID + 32]);
-      if (CUDA_NB_THREADS_PER_BLOCK >= 32) MINEQ(vol[threadID],vol[threadID + 16]);
-      if (CUDA_NB_THREADS_PER_BLOCK >= 16) MINEQ(vol[threadID],vol[threadID + 8]);
-      if (CUDA_NB_THREADS_PER_BLOCK >= 8)  MINEQ(vol[threadID],vol[threadID + 4]);
-      if (CUDA_NB_THREADS_PER_BLOCK >= 4)  MINEQ(vol[threadID],vol[threadID + 2]);
-      if (CUDA_NB_THREADS_PER_BLOCK >= 2)  MINEQ(vol[threadID],vol[threadID + 1]);
-    }
-    __syncthreads();
-}
-
 
 // Tableau en shared memory pour contenir la reduction locale
 __shared__ double local_min[CUDA_NB_THREADS_PER_BLOCK];
